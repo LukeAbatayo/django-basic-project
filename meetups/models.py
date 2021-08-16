013fa62228d0697cf937c45a8c84599efd01ae26
Luke Abatayo <47374947+LukeAbatayo@users.noreply.github.com>
2021-08-16 11:46:08 +0800

    added locations model, one to many relationship

--- meetups/models.py
+++ meetups/models.py
@@ -1,12 +1,21 @@
 from django.db import models
+from django.db.models.fields import CharField
 
 # Create your models here.
+
+class Location(models.Model):
+  name = models.CharField(max_length=200)
+  address = models.CharField(max_length=300)
+
+  def __str__(self):
+    return f'{self.name} ({self.address})'
 
 class Meetup(models.Model):
   title = models.CharField(max_length=200)
   slug = models.SlugField(unique=True)
   description = models.TextField()
-  image = models.ImageField(upload_to='images')\
+  image = models.ImageField(upload_to='images')
+  location = models.ForeignKey(Location, on_delete=models.CASCADE)
 
   def __str__(self):
     return f'{self.title} - {self.slug}'
