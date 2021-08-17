470fb2dc01f7c63e58f937ed7ec0f318fea1d450
Luke Abatayo <47374947+LukeAbatayo@users.noreply.github.com>
2021-08-17 22:58:06 +0800

    added ModelForm

--- meetups/views.py
+++ meetups/views.py
@@ -1,6 +1,8 @@
+from meetups.forms import RegistrationForm
 from django.shortcuts import render
 
 from .models import Meetup
+from .forms import RegistrationForm
 
 # Create your views here.
 
@@ -14,9 +16,11 @@ def index(request):
 def meetup_details(request, meetup_slug):
   try:
     selected_meetup = Meetup.objects.get(slug=meetup_slug)
+    Registration_form = RegistrationForm()
     return render(request, './meetups/meetup-details.html', {
       'meetup_found': True,
-      'meetup': selected_meetup
+      'meetup': selected_meetup,
+      'form': Registration_form
     })
   except Exception as exc:
     return render(request, './meetups/meetup-details.html', {
