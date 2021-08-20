52d07a617cbe44f5f493bafa5e69d3efb83d803f
Luke Abatayo <47374947+LukeAbatayo@users.noreply.github.com>
2021-08-20 23:40:41 +0800

    added ModelForm to Form

--- meetups/forms.py
+++ meetups/forms.py
@@ -1,10 +1,4 @@
 from django import forms
-from django.db import models
-from django.db.models import fields
 
-from .models import Participant
-
-class RegistrationForm(forms.ModelForm):
-  class Meta:
-    model = Participant
-    fields = ['email']
+class RegistrationForm(forms.Form):
+    email = forms.EmailField(label='Your email')
